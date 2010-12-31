e73b625ee581c25d40b5d5aac89fafac57803a91
cleziolima <cleziolima@12e8365d-0710-d3dc-1e3e-cce1ca2ccd26>
2010-12-31 11:54:11 +0000

    Implementacao de receitas

--- trunk/app/controllers/pages_controller.php
+++ trunk/app/controllers/pages_controller.php
@@ -80,11 +80,11 @@ class PagesController extends AppController {
         }
         
 
-        if($page == 'home'){
-            $this->layout = 'apresentacao';
-        }else{
-            $this->layout = 'cliente';
-        }
+//        if($page == 'home'){
+//            $this->layout = 'apresentacao';
+//        }else{
+//            $this->layout = 'cliente';
+//        }
         
         
         $this->set(compact('page', 'subpage', 'title'));
