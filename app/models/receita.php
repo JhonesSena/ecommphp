4c57df9757f0e994386da9eae34093745e44ed64
cleziolima <cleziolima@12e8365d-0710-d3dc-1e3e-cce1ca2ccd26>
2011-01-08 11:38:22 +0000

    View receita de cliente iniciada

--- app/models/receita.php
+++ app/models/receita.php
@@ -2,6 +2,7 @@
 class Receita extends AppModel {
 
 	var $name = 'Receita';
+        var $order = 'Receita.nome';
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $hasMany = array(
